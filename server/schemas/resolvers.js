b7f44ac07350c6d8c94df3553714013e5e3edcbd
Father Shark <11543424+brouiller@users.noreply.github.com>
2021-11-20 16:08:34 -0500

    updated saveBook mutation to update bookCount and changed bookCount scalar to Int from Integer

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,26 +1,68 @@
-const { User } = require('../models');
+const { User, Book } = require('../models');
+const { AuthenticationError } = require('apollo-server-express');
+const { signToken } = require('../utils/auth');
 
 const resolvers = {
   Query: {
-    getSingleUser: async (parent, { userId }) => {
-      return User.findOne({ _id: userId });
+    me: async (parent, args, context) => {
+      if (context.user) {
+        return User.findOne({ _id: context.user._id });
+      }
+      throw new AuthenticationError("Please log in.");
     },
   },
 
   Mutation: {
-    createUser: async (parent, { username, email, password }) => {
-      return User.create({ username, email, password });
+    addUser: async (parent, { username, email, password }) => {
+      const user = await User.create({ username, email, password });
+      const token = signToken(user);
+      return { token, user };
     },
-    removeThought: async (parent, { thoughtId }) => {
-      return Thought.findOneAndDelete({ _id: thoughtId });
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw new AuthenticationError("No user found with this email address");
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new AuthenticationError("Incorrect credentials");
+      }
+
+      const token = signToken(user);
+
+      return { token, user };
     },
-    removeComment: async (parent, { thoughtId, commentId }) => {
-      return Thought.findOneAndUpdate(
-        { _id: thoughtId },
-        { $pull: { comments: { _id: commentId } } },
-        { new: true }
+  },
+  saveBook: async (parent, args, context) => {
+    if (context.user) {
+      const book = await Book.create({ ...args });
+      const incrementBookCount = book.savedBooks.length();
+      await User.findOneAndUpdate(
+        { _id: context.user._id },
+        { $addToSet: { savedBooks: book.bookId, bookCount: incrementBookCount }, }
       );
-    },
+
+      return User;
+    }
+    throw new AuthenticationError("Please log in!");
+  },
+  removeBook: async (parent, args, context) => {
+    if (context.user) {
+      const book = await Book.findOneAndDelete({
+        bookId: args.bookId,
+      });
+
+      await User.findOneAndUpdate(
+        { _id: context.user._id },
+        { $pull: { savedBooks: book.bookId } }
+      );
+
+      return User;
+    }
+    throw new AuthenticationError("You need to be logged in!");
   },
 };
 
